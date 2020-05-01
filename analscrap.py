0ba86a8707040fd774e43b97f5cb0c92e0119fa9
ropeboyo <ropeshow@outlook.com>
2020-05-01 04:20:19 +0000

    more rectums SENPAIII

--- analscrap.py
+++ analscrap.py
@@ -1,12 +1,15 @@
 import requests
 from tinydb import TinyDB, Query
 
-start = 25256
+start = 139216
 target = 'anal'
 db = TinyDB(f"{target}.json")
 keep_going_senpai = True
 fail_count = 0
 format_options = ['jpg','png']
+factor = 1
+fail_tolerance = 64
+fc = 0
 
 while keep_going_senpai:
     test = str(hex(start)).replace('0x', '').upper()
@@ -17,17 +20,19 @@ while keep_going_senpai:
         r = requests.get(url)
         print(f"Target: {test} | Format: {option} | Status code: {r.status_code}")
         if r.status_code == 200:
-            #db.insert({'url': url, 'source': 'boob-bot'})
+            db.insert({'url': url, 'source': 'boob-bot'})
             found = True
+            """ fc = fc + 1
+            if fc >= 2:
+                keep_going_senpai = False """
     
     if found:
-        #fail_count = 0
-        keep_going_senpai = False
-    """ else:
+        fail_count = 0
+    else:
         fail_count = fail_count + 1
 
-    if fail_count > 2:
-        keep_going_senpai = False """
+    if fail_count > fail_tolerance:
+        keep_going_senpai = False
     
-    start = start + 1
+    start = start + factor
 
